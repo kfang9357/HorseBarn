25db043582931b63eda936c53d17c4af7f8a50fa
BT_1N3_27 <kfang9357@bths.edu>
2018-11-16 08:39:47 -0500

    2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,16 +7,16 @@ public class Main {
 	Horse horse2 = new Mustang("Dusty Trail", 22000);
 	Horse horse3 = newMustang("Silver", 12000);
 
-	Horse[] horses = new Horse [6];
+	Horse[] horses = new Horse [6]; //created new array for barn spaces
 
-	horse[1] = horse1;
-	horse[3] = horse2;
-	horse[5] = horse3;
+	horses[1] = horse1;
+	horses[3] = horse2;
+	horses[5] = horse3;
 
 	HorseBarn barn = new HorseBarn(horses);
 
 	System.out.println(barn);
-	System.out.prinln("Dusty Trail is in space: "+barn.findHorseSpace("Dusty Trail"));
+	System.out.println("Dusty Trail is in space: "+barn.findHorseSpace("Dusty Trail"));
 
 	barn.consolidate();
 

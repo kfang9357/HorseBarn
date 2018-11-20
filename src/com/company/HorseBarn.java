5c32ee6b6f8581449c23e27d8c18a9d432839323
kfang9357 <43207499+kfang9357@users.noreply.github.com>
2018-11-19 20:53:05 -0500

    4

--- src/com/company/HorseBarn.java
+++ src/com/company/HorseBarn.java
@@ -16,26 +16,34 @@ public class HorseBarn {
 
     public int findHorseSpace(String name) {
         for (int i = 0; i < this.spaces.length; i++) {
-            if (this.spaces[i]!=null && name.equals(this.spaces[i].getName()))
-            {
+            if (this.spaces[i] != null && name.equals(this.spaces[i].getName())) {
                 return i;
             }
-        return -1;
-    }
+            return -1;
+        }
 
-    public void consolidate()
-    {
-        for (int i = 0; i <this.spaces.length-1;i++)
-        {
-            if (this.spaces[i] ==null){
-                for (int j = i+1; j<this.spaces.length; j++){
-                    if (this.spaces[j] != null) {
-                        this.spaces[i] = this.spaces[j];
-                        this.spaces[j]=null;
-                        j = this.spaces.length;
+        public void consolidate() {
+            for (int i = 0; i < this.spaces.length - 1; i++) {
+                if (this.spaces[i] == null) {
+                    for (int j = i + 1; j < this.spaces.length; j++) {
+                        if (this.spaces[j] != null) {
+                            this.spaces[i] = this.spaces[j];
+                            this.spaces[j] = null;
+                            j = this.spaces.length;
+                        }
                     }
                 }
             }
         }
+    }
+
+    public String toString() {
+        String total = "";
+
+        for (int i = 0; i < this.spaces.length; i++) {
+            total += this.spaces[i] + " ";
+        }
+
+        return total;
     }
 }
